6f8264e7ea9f448b869476e86c95c6c6de899817
Zhanibek Khodzhabekov <zkhodzhabekov@bk.ru>
2019-05-14 18:08:59 +0600

    My first pushing from Netbeans!

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>My website</title>
+    </head>
+    
+    <body>
+        <?php
+            echo 'Hello world';
+        ?>
+    </body>
+    
+</html>
